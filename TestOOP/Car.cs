0471f83bbe3baa9fc02bf9606b85469de7a3b271
severli-admin <liza.sevryukova19@gmail.com>
2025-12-04 21:55:30 +0300

    initial

---
+++ TestOOP/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOP
+{
+    internal abstract class Car
+    {
+        int _gasoline;
+        int _gasolinePerKm;
+        public int _volume;
+
+        protected Car(int strartGasoline, int volume, int gasolinePerKm) 
+        {
+            _gasoline = strartGasoline;
+            _volume = volume;
+            _gasolinePerKm = gasolinePerKm;
+        }
+
+        public void Drive(int distance)
+        {
+            if (_gasoline > distance * _gasolinePerKm)
+            {
+                Console.WriteLine("Движется");
+            }
+            else
+            {
+                Console.WriteLine("Стоит");
+            }
+        }
+
+        public bool Refuel(int gasoline)
+        {
+            _gasoline = _volume;
+            return true;
+        }
+    }
+}
---
+++ TestOOP/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOP
+{
+    internal interface IVehicle
+    {
+        void Drive(int distance);
+        bool Refuel(int gasoline);
+    }
+}
