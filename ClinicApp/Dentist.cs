43747c13872131ad59b70c96c0dec88795a8bc1a
severli-admin <liza.sevryukova19@gmail.com>
2025-12-11 18:16:35 +0300

    initial commit

---
+++ ClinicApp/Dentist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6
+{
+    internal class Dentist : IDoctor
+    {
+        public void Treat()
+        {
+            Console.WriteLine("Dentist: вы здоровы, с вас 10000000000 денег.");
+        }
+    }
+}
---
+++ ClinicApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClinicApp;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Patient patient1 = new Patient("Liza");
+
+patient1.AddTreatmentPlan(new int[]{ 1, 2, 1, 3 });
+
+patient1.AppointDoctor();
+patient1.AppointDoctor();
