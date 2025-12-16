ea354ce62dc57822e132ea10287454b63f7d9fc0
severli-admin <liza.sevryukova19@gmail.com>
2025-12-16 16:14:50 +0300

    task 2

---
+++ hw7 try 2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using hw7_try_2;
+
+Computer comp1 = new Computer("HP", 1000);
+comp1.Info();
+
+Computer comp2 = new Computer("Lenovo", 1000, new RAM("ramName", 8), new HDD("hddName", 512, HDDType.externalHDD));
+comp2.Info();
+
