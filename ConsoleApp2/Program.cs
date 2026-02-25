5adffda7274d7748099b1f5f6a0cd9d624c82799
severli-admin <liza.sevryukova19@gmail.com>
2026-02-25 22:55:37 +0300

    initial hw 11

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp2;
+
+ComparablePair<int, string> AB = new ComparablePair<int, string>(1, "Lena");
+ComparablePair<int, string> CD = new ComparablePair<int, string>(1, "Gena");
+ComparablePair<int, string> EF = new ComparablePair<int, string>(2, "Liza");
+
+ComparablePair<int, string>[] pairs = { AB, CD, EF};
+Array.Sort(pairs);
+
+foreach (ComparablePair<int, string> p in pairs)
+{
+    Console.WriteLine(p);
+}
