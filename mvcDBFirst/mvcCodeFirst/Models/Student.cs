b37eff32fc5f301896ebb788f622d3bc8d1f2a42
Liza2 <liza.sevryukova19@gmail.com>
2026-03-12 14:41:38 +0100

    initial 23-25

---
+++ mvcDBFirst/mvcCodeFirst/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace mvcCodeFirst.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int? GroupId { get; set; }
+
+        public virtual Group? Group { get; set; }
+    }
+}
---
+++ mvcDBFirst/mvcDBFirst/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvcDBFirst.Models;
+
+public partial class Team
+{
+    public int TeamId { get; set; }
+
+    public string? TeamName { get; set; }
+
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+}
