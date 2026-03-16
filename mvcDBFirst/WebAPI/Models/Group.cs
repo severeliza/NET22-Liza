9cf99fd40e6b51e91da820b71c9cb48c7cd96e85
Liza2 <liza.sevryukova19@gmail.com>
2026-03-16 16:08:45 +0100

    hw 28 web api

---
+++ mvcDBFirst/WebAPI/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿namespace mvcCodeFirst.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; } = null!;
+
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
