9443d7b2ef25339e18cdceea28113e33165e83e0
Liza2 <liza.sevryukova19@gmail.com>
2026-04-01 14:58:48 +0200

    web api authorization

---
+++ mvcDBFirst/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("profile")]
+        public IActionResult GetProfile()
+        {
+            var username = User.FindFirst(System.Security.Claims.ClaimTypes.Name)?.Value;
+
+            return Ok(new
+            {
+                Message = "Доступ разрешен",
+                Username = username,
+                Data = "Секретные данные пользователя"
+            });
+        }
+
+        [HttpGet("public-data")]
+        [AllowAnonymous]
+        public IActionResult GetPublicData()
+        {
+            return Ok(new { Message = "Это видно всем даже без токена" });
+        }
+    }
+}
--- mvcDBFirst/WebAPI/Program.cs
+++ mvcDBFirst/WebAPI/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using mvcCodeFirst.Models;
+using System.Reflection;
+using System.Text;
 using WebAPI.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,14 +14,49 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
+
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+//    // 
+//    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+//    c.IncludeXmlComments(xmlPath);
+//});
+
+var cts = new CancellationTokenSource();
+
 
 builder.Services.AddDbContext<CourseDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        //ValidIssuer = jwtSettings["Issuer"],
+        //ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -34,6 +74,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapCourseEndpoints();
+//app.MapCourseEndpoints();
 
 app.Run();
